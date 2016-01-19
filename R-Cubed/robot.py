529c15fdcfeda0d7908664428ca8404b28e150b3
nikolojedison <nikolojedison@gmail.com>
2016-01-19 16:58:59 -0600

    Camera

--- R-Cubed/robot.py
+++ R-Cubed/robot.py
@@ -6,21 +6,28 @@ class RCubed(wpilib.IterativeRobot):
 
     def robotInit(self):
         """This initialises the robot."""
-        
+
         self.robot_drive = wpilib.RobotDrive(0,1)
         self.stick = wpilib.Joystick(0)
         self.hall = wpilib.DigitalInput(0)
         self.analog_hall = wpilib.AnalogInput(0)
         self.spike = wpilib.Relay(1)
-        
+        camera = wpilib.USBCamera()
+        camera.setExposureManual(50)
+        camera.setBrightness(80)
+        camera.updateSettings()
+
+        server = wpilib.CameraServer.getInstance()
+        server.startAutomaticCapture(camera)
+
     def autonomousInit(self):
         """This gets run once when the robot enters auton."""
         pass
-    
+
     def autonomousPeriodic(self):
         """This gets called periodically during auton."""
         pass
-    
+
     def teleopPeriodic(self):
         """This gets called periodically during teleop."""
         self.robot_drive.arcadeDrive(self.stick)
@@ -28,10 +35,8 @@ class RCubed(wpilib.IterativeRobot):
             self.spike.set(self.spike.Value.kForward)
         else:
             self.spike.set(self.spike.Value.kOff)
-            
+
     def disabledPeriodic(self):
-        self.robot_drive.arcadeDrive(self.stick)
-        print({"Analog Hall": self.analog_hall.get(), "Digital Hall": self.hall.get()})
-    
+        pass
 if __name__ == "__main__":
     wpilib.run(RCubed)
